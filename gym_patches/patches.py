6c225c79bb4f1fefc51ae81e925af2022710e97e
Amir Tadrisi <amirtds@gmail.com>
2024-08-29 13:18:57 -0500

    feat: updare to redwood

--- gym_patches/patches.py
+++ gym_patches/patches.py
@@ -6,10 +6,13 @@ from opaque_keys.edx.keys import CourseKey
 from openedx.core.lib.courses import get_course_by_id
 from lms.djangoapps.certificates import api as certs_api
 from lms.djangoapps.courseware.views.views import is_course_passed
+from django.utils.translation import gettext_lazy as _
 # from lms.djangoapps.certificates.exceptions import CertificateGenerationNotAllowed
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from openedx.core.djangoapps.user_authn.views.register import _track_user_registration as original_track_user_registration
 from common.djangoapps.course_modes.models import CourseMode
+from xmodule.modulestore.django import modulestore
+
 from django.conf import settings
 import datetime
 from pytz import UTC
@@ -18,91 +21,82 @@ from common.djangoapps.track import segment
 
 logger = logging.getLogger(__name__)
 
-# def custom_generate_user_cert(request, course_id):
-#     """
-#     Request that a course certificate be generated for the user.
+@require_POST
+@transaction.non_atomic_requests
+def custom_generate_user_cert(request, course_id):
+    """
+    Request that a course certificate be generated for the user.
 
-#     In addition to requesting generation, this method also checks for and returns the certificate status.
-#     Note that because generation is an asynchronous process, the certificate may not have been generated when
-#     its status is retrieved.
+    Args:
+        request (HttpRequest): The POST request to this view.
+        course_id (unicode): The identifier for the course.
+    Returns:
+        HttpResponse: 200 on success, 400 if a new certificate cannot be generated.
+    """
+    if not request.user.is_authenticated:
+        logger.info("Anon user trying to generate certificate for %s", course_id)
+        return HttpResponseBadRequest(
+            _('You must be signed in to {platform_name} to create a certificate.').format(
+                platform_name=configuration_helpers.get_value('PLATFORM_NAME', settings.PLATFORM_NAME)
+            )
+        )
 
-#     Args:
-#         request (HttpRequest): The POST request to this view.
-#         course_id (unicode): The identifier for the course.
-#     Returns:
-#         HttpResponse: 200 on success, 400 if a new certificate cannot be generated.
-#     """
-#     if not request.user.is_authenticated:
-#         logger.info("Anon user trying to generate certificate for %s", course_id)
-#         return HttpResponseBadRequest(
-#             _('You must be signed in to {platform_name} to create a certificate.').format(
-#                 platform_name=configuration_helpers.get_value('PLATFORM_NAME', settings.PLATFORM_NAME)
-#             )
-#         )
+    student = request.user
+    course_key = CourseKey.from_string(course_id)
 
-#     student = request.user
-#     course_key = CourseKey.from_string(course_id)
-#     course = get_course(course_id)
+    try:
+        course = get_course_by_id(course_key)
+    except Exception:
+        return HttpResponseBadRequest(_("Course is not valid"))
 
-#     if not course:
-#         return HttpResponseBadRequest(_("Course is not valid"))
+    if not is_course_passed(course, student):
+        return HttpResponseBadRequest(_("Your certificate will be available when you pass the course."))
 
-#     logger.info(f'Attempt will be made to generate a course certificate for {student.id} : {course_key}.')
+    certificate_status = certs_api.certificate_downloadable_status(student, course.id)
 
-#     try:
-#         certs_api.generate_certificate_task(student, course_key, 'self')
-#     except CertificateGenerationNotAllowed as e:
-#         logger.exception(
-#             "Certificate generation not allowed for user %s in course %s",
-#             str(student),
-#             course_key,
-#         )
-#         return HttpResponseBadRequest(str(e))
+    if certificate_status["is_downloadable"]:
+        return HttpResponseBadRequest(_("Certificate has already been created."))
+    elif certificate_status["is_generating"]:
+        return HttpResponseBadRequest(_("Certificate is being created."))
 
-#     if not is_course_passed(student, course):
-#         logger.info("User %s has not passed the course: %s", student.username, course_id)
-#         return HttpResponseBadRequest(_("Your certificate will be available when you pass the course."))
+    logger.info(
+        "Attempt will be made to generate a course certificate for student %s in course %s",
+        student.id,
+        course_key
+    )
 
-#     certificate_status = certs_api.certificate_downloadable_status(student, course.id)
+    try:
+        certs_api.generate_user_certificates(student, course_key, course=course)
+    except Exception as exc:
+        logger.critical(
+            "Certificate generation failed for user %s, course %s with error: %s",
+            student.id,
+            course_key,
+            str(exc)
+        )
+        return HttpResponseBadRequest(_("An error occurred while creating your certificate."))
 
-#     logger.info(
-#         "User %s has requested for certificate in %s, current status: is_downloadable: %s, is_generating: %s",
-#         student.username,
-#         course_id,
-#         certificate_status["is_downloadable"],
-#         certificate_status["is_generating"],
-#     )
+    return HttpResponse()
 
-#     # Track certificate generation request with Segment
-#     if hasattr(settings, 'LMS_SEGMENT_KEY') and settings.LMS_SEGMENT_KEY:
-#         traits = {
-#             'email': student.email,
-#             'username': student.username,
-#             'course_id': course_id,
-#             'is_downloadable': certificate_status["is_downloadable"],
-#             'is_generating': certificate_status["is_generating"]
-#         }
-#         segment.track(student.id, "Certificate Generation Requested", traits)
-
-#     if certificate_status["is_downloadable"]:
-#         return HttpResponseBadRequest(_("Certificate has already been created."))
-#     elif certificate_status["is_generating"]:
-#         return HttpResponseBadRequest(_("Certificate is being created."))
-
-#     return HttpResponse()
-
-# # Existing functions
 
 def custom_track_user_registration(user, profile, params, third_party_provider, registration, is_marketable):
-    """ Track the user's registration with custom market field. """
+    """
+    Custom version of track_user_registration with additional market field tracking.
+    This function is intended to be used as a monkey patch for the original _track_user_registration.
+    """
+    # First, call the original function to maintain base functionality
+    original_track_user_registration(user, profile, params, third_party_provider, registration, is_marketable)
+
+    # Now add our custom tracking logic
     if hasattr(settings, 'LMS_SEGMENT_KEY') and settings.LMS_SEGMENT_KEY:
         try:
             market = user.extrainfo.market
-            extrainfo = {}
-            extrainfo['market'] = market
+            extrainfo = {'market': market}
         except Exception as e:
-            logger.exception("Exception in extrainfo_dict: {}".format(e))
+            logger.exception("Exception in extrainfo_dict: %s", e)
             extrainfo = ''
+
+        # Update traits with our custom data
         traits = {
             'email': user.email,
             'username': user.username,
@@ -114,55 +108,30 @@ def custom_track_user_registration(user, profile, params, third_party_provider, 
             'gender': profile.gender_display,
             'country': str(profile.country),
             'is_marketable': is_marketable,
-            'extrainfo': extrainfo  # Add the market field to the traits
+            'extrainfo': extrainfo
         }
 
-        if settings.MARKETING_EMAILS_OPT_IN and params.get('marketing_emails_opt_in'):
-            email_subscribe = 'subscribed' if is_marketable else 'unsubscribed'
-            traits['email_subscribe'] = email_subscribe
-
+        # Identify the user with the updated traits
         segment.identify(user.id, traits)
-        properties = {
-            'category': 'conversion',
-            'email': user.email,
-            'label': params.get('course_id'),
-            'provider': third_party_provider.name if third_party_provider else None,
-            'is_gender_selected': bool(profile.gender_display),
-            'is_year_of_birth_selected': bool(profile.year_of_birth),
-            'is_education_selected': bool(profile.level_of_education_display),
-            'is_goal_set': bool(profile.goals),
-            'total_registration_time': round(float(params.get('totalRegistrationTime', '0'))),
-            'activation_key': registration.activation_key if registration else None,
-            'host': params.get('host', ''),
-            'utm_campaign': params.get('utm_campaign', ''),
-        }
 
-        if params.get('marketing_emails_opt_in') and settings.MARKETING_EMAILS_OPT_IN:
-            properties['marketing_emails_opt_in'] = is_marketable
-
-        segment_traits = dict(properties)
-        segment_traits['user_id'] = user.id
-        segment_traits['joined_date'] = user.date_joined.strftime("%Y-%m-%d")
+        # Track the custom event
         segment.track(
             user.id,
-            "edx.bi.user.account.registered",
-            properties=properties,
-            traits=segment_traits,
+            "edx.bi.user.account.registered.custom",
+            properties={
+                'category': 'conversion',
+                'email': user.email,
+                'username': user.username,
+                'market': market if extrainfo else None
+            }
         )
 
 def custom_is_eligible_for_certificate(cls, mode_slug, status=None):
     """
-    Custom version of the method without excluding AUDIT mode.
+    Custom version of the method that considers all modes eligible for certificate.
     """
     logger.info(f"Custom is_eligible_for_certificate called with mode_slug: {mode_slug}")
-    ineligible_modes = []
-
-    if settings.FEATURES.get('DISABLE_HONOR_CERTIFICATES', False):
-        from lms.djangoapps.certificates.data import CertificateStatuses
-        if mode_slug == cls.HONOR and status != CertificateStatuses.downloadable:
-            ineligible_modes.append(cls.HONOR)
-
-    return mode_slug not in ineligible_modes
+    return True
 
 def apply_monkey_patch():
     # Apply monkey patch for is_eligible_for_certificate
@@ -174,5 +143,5 @@ def apply_monkey_patch():
     register._track_user_registration = custom_track_user_registration
     
     # Apply the patch for generate_user_cert
-    # from lms.djangoapps.courseware.views import views
-    # views.generate_user_cert = custom_generate_user_cert
+    from lms.djangoapps.courseware.views import views
+    views.generate_user_cert = custom_generate_user_cert
